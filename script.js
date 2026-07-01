1f4885496098154cf1a3f12322c904faaf3baa9c
oobergoober000-png <oobergoober000@gmail.com>
2026-07-01 14:30:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const dearContent = "Dear xxx,";
+const closingContent = "\nWith all my love,\n[Your Name]";
+
+const input = document.getElementById('password');
+
+input.addEventListener('input', (e) => {
+    let val = e.target.value.replace(/\D/g, '');
+    if (val.length > 2 && val.length <= 4) {
+        val = val.slice(0, 2) + '/' + val.slice(2);
+    } else if (val.length > 4) {
+        val = val.slice(0, 2) + '/' + val.slice(2, 4) + '/' + val.slice(4);
+    }
+    e.target.value = val;
+});
+
+function showHint() {
+    document.getElementById('hint-text').style.display = 'block';
+}
+
+function checkPassword() {
+    const cleanInput = document.getElementById('password').value.replace(/\D/g, '');
+    if (cleanInput === "01222023") {
+        document.getElementById('login').style.display = 'none';
+
+        const content = document.getElementById('content');
+        content.style.display = 'flex';
+
+        typeWriter("dear-area", dearContent, 0, 100, () => {
+            document.getElementById("body-area").style.opacity = '1';
+            setTimeout(() => {
+                typeWriter("closing-area", closingContent, 0, 100, null);
+            }, 3000);
+        });
+    } else {
+        alert("Incorrect date.");
+    }
+}
+
+function typeWriter(id, text, i, speed, callback) {
+    if (i < text.length) {
+        const ch = text.charAt(i) === '\n' ? '<br>' : text.charAt(i);
+        document.getElementById(id).innerHTML += ch;
+        setTimeout(() => typeWriter(id, text, i + 1, speed, callback), speed);
+    } else if (callback) {
+        callback();
+    }
+}
