a967319346742d6bf40ed706a0a4888b01d277e8
oobergoober000-png <oobergoober000@gmail.com>
2026-07-01 14:59:44 -0400

    Refactor showHint function for hint image animation
    Updated showHint function to include animation for hint image.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function showHint() {
     // Show hint text
     hintText.style.display = 'block';
 
-    // Show and animate the hint image
+    // Show and animate the hint image inside the yellow box
     hintImage.classList.add('hint-visible');
 }
 
